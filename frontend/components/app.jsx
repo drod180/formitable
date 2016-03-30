6484d1ca9c2378f551768367e6454ec7797ca24e
drod180 <719drod@gmail.com>
2016-03-30 19:56:16 -0400

    Add css formatting for navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,14 +3,14 @@ var React = require('react');
 var App = React.createClass({
   render: function () {
     return (
-      <div>
-        <header>
+      <div className="app-body">
+        <header className="navbar group">
           <img className="logo-navbar" />
-          <ul>
-            <li><a href="#">Forms</a></li>
-            <ul>Account
-              <li><a href="#">My Account</a></li>
-              <li><a href="#">Sign Out</a></li>
+          <ul className="navbar-menu group">
+            <li className="navbar-item"><a href="#">Forms</a></li>
+            <ul className="navbar-item account"><a href="#">Account</a>
+              <li className="account-item"><a href="#">My Account</a></li>
+              <li className="account-item"><a href="#">Sign Out</a></li>
             </ul>
           </ul>
         </header>
