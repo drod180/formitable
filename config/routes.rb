64bf728e6c781875691d86cab84c22da4c72f7a9
drod180 <719drod@gmail.com>
2016-03-30 09:45:46 -0400

    Adjusted webpack and Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#root'
+  
   resources :users, only: [:new, :create, :update]
   resource :session, only: [:new, :create, :destroy]
 
---
+++ frontend/formitable.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var IndexRoute = require('react-router').IndexRoute;
+
+var App = React.createClass({
+  render: function () {
+    return (<div>Become Formitable!</div>);
+  }
+});
+
+var routes = (
+  <Route path="/" component={App}></Route>
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+  ReactDOM.render(
+    <Router>{routes}</Router>,
+    document.getElementById('root')
+  );
+});
